21828f8d3756e87a5d9b675a4694fcffe60a3471
ibrada <radikibragimov@mail.ru>
2025-03-10 01:35:13 +0300

    Done

---
+++ src/EmployeeBook.java
@@ -0,0 +1,64 @@
+public class EmployeeBook {
+
+    private static final Employee[] EMPLOYEES = new Employee[10];
+
+    public EmployeeBook() {
+    }
+
+    public static Employee[] getEmployees() {
+        return EMPLOYEES;
+    }
+
+    public void print() {
+        for (int i = 0; i < EMPLOYEES.length; i++) {
+            System.out.println(EMPLOYEES[i]);
+        }
+    }
+
+    public void printFullName() {
+        for (int i = 0; i < EmployeeBook.EMPLOYEES.length; i++) {
+            System.out.println(EmployeeBook.EMPLOYEES[i].getFullName());
+        }
+    }
+
+    public int sumSalary() {
+        int sum = 0;
+        for (int i = 0; i < EMPLOYEES.length; i++) {
+            sum = sum + EMPLOYEES[i].getSalary();
+        }
+        return sum;
+    }
+
+    public Employee findEmployeeWithMaxSalary() {
+        Employee employeeWithMaxSalary = null;
+        int maxSalary = Integer.MIN_VALUE;
+        for (int i = 0; i < EMPLOYEES.length; i++) {
+            if (EMPLOYEES[i].getSalary() > maxSalary) {
+                maxSalary = EMPLOYEES[i].getSalary();
+                employeeWithMaxSalary = EMPLOYEES[i];
+            }
+        }
+        return employeeWithMaxSalary;
+    }
+
+    public Employee findEmployeeWithMinSalary() {
+        Employee employeeWithMinSalary = null;
+        int minSalary = Integer.MAX_VALUE;
+        for (int i = 0; i < EMPLOYEES.length; i++) {
+            if (EMPLOYEES[i].getSalary() < minSalary) {
+                minSalary = EMPLOYEES[i].getSalary();
+                employeeWithMinSalary = EMPLOYEES[i];
+            }
+        }
+        return employeeWithMinSalary;
+    }
+
+    public int averageSalary() {
+        if (EMPLOYEES.length == 0) {
+            return 0;
+        }
+        return sumSalary() / EMPLOYEES.length;
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,78 +1,40 @@
 public class Main {
 
-    private static Employee[] employees = new Employee[10];
+    private static void line() {
+        System.out.println("* * *");
+    }
 
     public static void main(String[] args) {
 
-        employees[0] = new Employee("Ivan", 1, 10000);
-        employees[1] = new Employee("Petr", 2, 20000);
-        employees[2] = new Employee("Mike", 3, 11000);
-        employees[3] = new Employee("David", 4, 15000);
-        employees[4] = new Employee("Jack", 5, 12000);
-        employees[5] = new Employee("Jane", 1, 18000);
-        employees[6] = new Employee("Bob", 2, 15000);
-        employees[7] = new Employee("John", 3, 16000);
-        employees[8] = new Employee("Mike", 4, 17000);
-        employees[9] = new Employee("David", 5, 13000);
+        EmployeeBook employeeBook = new EmployeeBook();
 
-        print();
-        printFullName();
-        System.out.println(sumSalary());
-        System.out.println(findEmployeeWithMaxSalary());
-        System.out.println(findEmployeeWithMinSalary());
-        System.out.println(averageSalary());
+        EmployeeBook.getEmployees()[0] = new Employee("Ivan", 1, 10000);
+        EmployeeBook.getEmployees()[1] = new Employee("Petr", 2, 20000);
+        EmployeeBook.getEmployees()[2] = new Employee("Mike", 3, 11000);
+        EmployeeBook.getEmployees()[3] = new Employee("David", 4, 15000);
+        EmployeeBook.getEmployees()[4] = new Employee("Jack", 5, 12000);
+        EmployeeBook.getEmployees()[5] = new Employee("Jane", 1, 18000);
+        EmployeeBook.getEmployees()[6] = new Employee("Bob", 2, 15000);
+        EmployeeBook.getEmployees()[7] = new Employee("John", 3, 16000);
+        EmployeeBook.getEmployees()[8] = new Employee("Mike", 4, 17000);
+        EmployeeBook.getEmployees()[9] = new Employee("David", 5, 13000);
+
+        employeeBook.print();
+        line();
+        employeeBook.printFullName();
+        line();
+        System.out.println(employeeBook.sumSalary());
+        line();
+        System.out.println(employeeBook.findEmployeeWithMaxSalary());
+        line();
+        System.out.println(employeeBook.findEmployeeWithMinSalary());
+        line();
+        System.out.println(employeeBook.averageSalary());
     }
 
-    private static void print() {
-        for (int i = 0; i < employees.length; i++) {
-            System.out.println(employees[i]);
-        }
-    }
 
-    private static void printFullName() {
-        for (int i = 0; i < employees.length; i++) {
-            System.out.println(employees[i].getFullName());
-        }
-    }
 
-    private static int sumSalary() {
-        int sum = 0;
-        for (int i = 0; i < employees.length; i++) {
-            sum = sum + employees[i].getSalary();
-        }
-        return sum;
-    }
 
-    private static Employee findEmployeeWithMaxSalary() {
-        Employee employeeWithMaxSalary = null;
-        int maxSalary = Integer.MIN_VALUE;
-        for (int i = 0; i < employees.length; i++) {
-            if (employees[i].getSalary() > maxSalary) {
-                maxSalary = employees[i].getSalary();
-                employeeWithMaxSalary = employees[i];
-            }
-        }
-        return employeeWithMaxSalary;
-    }
-
-    private static Employee findEmployeeWithMinSalary() {
-        Employee employeeWithMinSalary = null;
-        int minSalary = Integer.MAX_VALUE;
-        for (int i = 0; i < employees.length; i++) {
-            if (employees[i].getSalary() < minSalary) {
-                minSalary = employees[i].getSalary();
-                employeeWithMinSalary = employees[i];
-            }
-        }
-        return employeeWithMinSalary;
-    }
-
-    private static int averageSalary() {
-        if (employees.length == 0) {
-            return 0;
-        }
-        return sumSalary() / employees.length;
-    }
 
 
 
